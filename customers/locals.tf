1fdec6b7d2d030e9361cee4e8821a41a754bb2cc
Allan Hardy <al_hardy@live.com.au>
2021-07-12 23:02:17 +1000

    fix alb host header per env

--- customers/locals.tf
+++ customers/locals.tf
@@ -1,7 +1,7 @@
 locals {
-  tfc_workspace_slug_parts           = split("-", var.TFC_WORKSPACE_SLUG)
-  env                                = element(local.tfc_workspace_slug_parts, length(local.tfc_workspace_slug_parts) - 1)
-  service_name = "customers-api"
-  container_name = "service-customers-api"
-  aws_alb_listener_rule_host_header = env == "prod" ? "${lower(local.service_name)}.alhardy.net" : "${lower(local.service_name)}.${local.env}.alhardy.net"
+  tfc_workspace_slug_parts          = split("-", var.TFC_WORKSPACE_SLUG)
+  env                               = element(local.tfc_workspace_slug_parts, length(local.tfc_workspace_slug_parts) - 1)
+  service_name                      = "customers-api"
+  container_name                    = "service-customers-api"
+  aws_alb_listener_rule_host_header = local.env == "prod" ? "${lower(local.service_name)}.alhardy.net" : "${lower(local.service_name)}.${local.env}.alhardy.net"
 }
